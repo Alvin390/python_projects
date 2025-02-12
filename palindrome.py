19fd9f91330e8f2b68e135f1dd679946bd88c2a3
Alvin Kariuki <68065867+Alvin390@users.noreply.github.com>
2025-02-12 20:30:37 +0300

    Delete palindrome.py

--- palindrome.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def palindrome(self):
-        x=(input("Enter Number"))
-        if x==x[::-1]:
-            print('Palindrome')
-        else:
-            print('Not Palindrome')
-test1=Solution()
-test1.palindrome()
